d69af0a228841a575ea5b9ff0872055dd0d9d208
aka <aka@opencascade.com>
2020-08-11 18:15:07 +0300

    change/clear root component title of application was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-path';
+  pageTitle = 'Acme Product Management';
 }
