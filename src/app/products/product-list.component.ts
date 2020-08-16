97046c6f1a71b3de625d103ea6a13856c6f8e76e
aka <aka@opencascade.com>
2020-08-16 14:02:31 +0300

    More on components: interfaces, custom pipes, implements, get\set, filtering, lifesycle hooks, styles

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,31 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PRODUCTS } from './products.data';
-// import { IProduct } from './product';
+import { IProduct } from './product';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
-  // styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
 
   public pageTitle: string;
   public imageWidth: number;
   public imageMargin: number;
   public isShowImage: boolean;
-  public products: Array<any>;
-  public listFilter: string;
+  public products: Array<IProduct>;
+  public filteredProducts: Array<IProduct>;
+
+  private _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this._performFilterProducts() : this.products;
+  }
 
   constructor() {
     this.pageTitle = 'Product List';
     this.imageWidth = 50;
     this.imageMargin = 2;
     this.isShowImage = false;
-    this.listFilter = 'cart';
     this.products = new Array<any>(...PRODUCTS);
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
+
+  ngOnInit(): void {
   }
 
   onShowImageClick(): void {
     this.isShowImage = !this.isShowImage;
+  }
+
+  private _performFilterProducts() {
+    return this.products.filter(
+      (product: IProduct) => product.productName.toLowerCase().includes(this.listFilter.toLowerCase()));
   }
 }
