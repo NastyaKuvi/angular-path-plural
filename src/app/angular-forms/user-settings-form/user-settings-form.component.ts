a2f9b1b0194f5ba1c53d46fde21964dd26dbfa9b
aka <aka@opencascade.com>
2020-08-17 22:07:31 +0300

    form-validation: example on first input

--- src/app/angular-forms/user-settings-form/user-settings-form.component.ts
+++ src/app/angular-forms/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IUserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'af-user-settings-form',
@@ -25,4 +26,11 @@ export class UserSettingsFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm) {
+    console.log('on submit: ', form.valid);
+  }
+
+  onBlur(field: NgModel) {
+    console.log('on blur: ', field.valid);
+  }
 }
